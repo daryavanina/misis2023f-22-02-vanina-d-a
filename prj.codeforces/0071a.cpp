e3253dc08a427d3b7aea46f14ffb471efade24aa
Darya <dashavanina04.dv@gmail.com>
2023-10-10 23:50:50 +0300

    add 0071a.cpp

---
+++ prj.codeforces/0071a.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<string>
+
+int main()
+{
+	int n = 0;
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		std::string s;
+		std::cin >> s;
+		int n = 0;
+		n = s.length();
+		if (n > 10) {
+			std::cout << s[0] + std::to_string(n - 2) + s[n - 1] << std::endl;
+		}
+		else {
+			std::cout << s << std::endl;
+		}
+	}
+}
